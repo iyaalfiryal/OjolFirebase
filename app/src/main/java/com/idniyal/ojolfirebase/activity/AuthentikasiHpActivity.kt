fb34fa2d1a6d5e50dec14edba31f2e6691d64ef6
fifihuwaida@gmail.com <fifihuwaida@gmail.com>
2020-08-23 11:58:10 +0700

    Initial commit

---
+++ app/src/main/java/com/idniyal/ojolfirebase/activity/AuthentikasiHpActivity.kt
@@ -0,0 +1,12 @@
+package com.idniyal.ojolfirebase.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AuthentikasiHpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentikasi_hp)
+    }
+}
---
+++ app/src/main/java/com/idniyal/ojolfirebase/utils/Constan.kt
@@ -0,0 +1,12 @@
+package com.idniyal.ojolfirebase.utils
+
+//todo 4
+object Constan {
+
+    //untuk menyimpan data string static
+    const val tb_uaser = "User"
+    val tb_booking = "Booking"
+    val baseUrlRoute = "https://maps.googleapis.com/maps/api/directions/"
+    val baseUrlFcm = "https://fcm.googleapis.com"
+    val key = "key"
+}
